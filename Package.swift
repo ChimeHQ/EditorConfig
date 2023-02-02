69a3e5d5801f7b7313966fd2434b5e3ca12faff8
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-02-02 07:49:54 -0500

    Explicit platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
 	name: "EditorConfig",
+	platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
 	products: [
 		.library(name: "EditorConfig", targets: ["EditorConfig"]),
 	],
